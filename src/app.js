4115d886a64d40d6668de071eef608356c8c25c9
Student ApnaCollege <133749957+bhupendrasinghpatel@users.noreply.github.com>
2026-02-27 13:20:49 +0530

    Adding

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookiesParser from "cookie-parser"
-
+import userRouter from "./routes/user.routes.js"
 const app = express()
 
 app.use(cors({
@@ -14,5 +14,8 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.use("/api/v1/users", userRouter)
+
+//http://localhost:8000/api/v1/users/register
 export {app}
 
